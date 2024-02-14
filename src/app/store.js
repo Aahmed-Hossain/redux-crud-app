cf133402dec9ff6f78d8915728963c68378f58d2
Aahmed-Hossain <aahmdctec@gmail.com>
2024-02-14 21:13:51 +0600

    installed the redux-persist to persist the data after reloading

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
-import  booksSlice  from '../components/BooksSlice';
+// import { configureStore } from '@reduxjs/toolkit'
+// import  booksSlice  from '../components/BooksSlice';
+
+
+// const store = configureStore({
+//     reducer:{
+//         booksReducer: booksSlice
+//     }
+// });
+// export default store;
+
+import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import booksSlice from '../components/BooksSlice';
+
+const persistConfig = {
+  key: 'root', // the key to use for storing the data in storage
+  storage, // storage engine
+};
+
+const persistedReducer = persistReducer(persistConfig, booksSlice);
 
 const store = configureStore({
-    reducer:{
-        booksReducer: booksSlice
-    }
+  reducer: {
+    booksReducer: persistedReducer,
+  },
 });
-export default store;
+
+const persistor = persistStore(store);
+
+export { store, persistor };
